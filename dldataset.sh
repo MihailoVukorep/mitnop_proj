7d66a0b244b06f41d91a0e8b96ce7f97fe8fbea1
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-04-30 20:43:45 +0200

    more setup stuff

---
+++ dldataset.sh
@@ -0,0 +1,4 @@
+# https://www.nist.gov/itl/products-and-services/emnist-dataset
+
+mkdir datasets
+wget --show-progress https://biometrics.nist.gov/cs_links/EMNIST/gzip.zip -P datasets/.
--- setup.sh
+++ setup.sh
@@ -3,6 +3,3 @@ python -m virtualenv p3env
 source p3env/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-
-mkdir datasets
-#wget <add link> datasets/.
