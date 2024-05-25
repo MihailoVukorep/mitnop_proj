831e6e39db5b1cfc41c762e2ce009755672c6909
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-05-25 18:00:55 +0200

    added valid set cleanup script

--- dev/dataset_cleanup.py
+++ dev/dataset_cleanup.py
@@ -9,27 +9,10 @@ from utils import *
 #dupes_check_all()
 
 # %% load set
-def unique(images, labels, mapping):
-    print(f'images.: {images.shape}')
-    print(f'labels.: {labels.shape}')
-    print(f'class..: {len(mapping)}')
-    print(f"bytes..: {bytes_human_readable(images.nbytes)}")
-
-    print("removing duplicates...")
-    images, labels = dupes_rm(images, labels)
-    print("dupes removed.")
-
-    print("unique images:")
-    print(f'images.: {images.shape}')
-    print(f"bytes..: {bytes_human_readable(images.nbytes)}")
-
-    mapping_arr = np.array(list(mapping))
-    np.save('datasets/cleanset_all_images.npy', images)
-    np.save('datasets/cleanset_all_labels.npy', labels)
-    np.save('datasets/cleanset_all_mapping.npy', mapping_arr)
-
-# %% unique all
 images, labels, mapping = dataset_load_all()
-unique(images, labels, mapping)
+mapping_arr = np.array(list(mapping))
+np.save(d_datasets('cleanset_all_images.npy'), images)
+np.save(d_datasets('cleanset_all_labels.npy'), labels)
+np.save(d_datasets('cleanset_all_mapping.npy'), mapping_arr)
 
-
+# TODO: valid train test split
