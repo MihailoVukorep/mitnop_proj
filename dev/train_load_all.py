1592553947204bec2d72811952e9878d401a8460
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-05-25 18:54:26 +0200

    training better model

--- dev/train_load_all.py
+++ dev/train_load_all.py
@@ -18,21 +18,32 @@ print()
 
 # %% preprocess set for tf
 print("preparing images for tensorflow...")
-train_input, train_target = prepdata(images, labels)
-
+images_tf, labels_tf = prepdata(images, labels)
 print("images prepared.")
+
+
+# %% train val split
+
+train_input, test_input, train_target, test_target = train_test_split(images_tf, labels_tf, test_size=0.2, random_state=42)
 
 # %% create model
 print("creating model:")
 model = create_model()
 
-# %% training model params
-num_epochs = 4
-batch_size = 100
+# %% training basic model
 
-# %% train model
+num_epochs = 5
+batch_size = 64
+
 print("training model...")
-cnn_results = model.fit(train_input, train_target, batch_size=batch_size, epochs=num_epochs, verbose=2)
+history = model.fit(
+    train_input,
+    train_target,
+    batch_size=batch_size,
+    validation_data=(test_input, test_target),
+    epochs=num_epochs,
+    verbose=2
+)
 print("model trained.")
 
 # %% save model
@@ -40,3 +51,35 @@ print("saving model...")
 model.save(d_models('model_all.keras'))
 print("model saved.")
 
+
+
+
+# # %% training a super model
+# num_epochs = 50
+# batch_size = 64
+
+# datagen = ImageDataGenerator(rotation_range=10, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.1, zoom_range=0.1)
+
+# def lr_scheduler(epoch, lr):
+#     if epoch % 10 == 0 and epoch != 0:
+#         lr = lr * 0.9
+#     return lr
+
+# lr_callback = LearningRateScheduler(lr_scheduler)
+# print("training model...")
+# history = model.fit(
+#     datagen.flow(train_input, train_target, batch_size=batch_size),
+#     steps_per_epoch=len(train_input) // batch_size,
+#     validation_data=(test_input, test_target),
+#     epochs=num_epochs,
+#     callbacks=[lr_callback],
+#     verbose=2
+# )
+# print("model trained.")
+
+# # %% save model
+# print("saving model...")
+# model.save(d_models('model_all.keras'))
+# print("model saved.")
+
+# %%
--- dev/utils.py
+++ dev/utils.py
@@ -16,6 +16,11 @@ from sys import getsizeof
 from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, Input, MaxPooling2D
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 class_mapping = {
     '0': 0,
@@ -381,7 +386,7 @@ def dataset_info():
 
 
 
-def create_model():
+def create_model_old():
     model = Sequential()
     model.add(Input(shape=(28, 28, 1)))
     model.add(Conv2D(filters=16, kernel_size=(5,5), strides=(2,2), padding="valid", activation="relu", use_bias=True))
@@ -395,10 +400,28 @@ def create_model():
     model.summary()
     return model
 
+def create_model():
+    model = Sequential()
+    model.add(Input(shape=(28, 28, 1)))
+    model.add(Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dense(class_mapping_n, activation="softmax"))
+    model.compile(loss="categorical_crossentropy", optimizer=Adam(), metrics=["accuracy"])
+    model.summary()
+    return model
 
 def prepdata(images, labels):
     target_labels = np.array([class_mapping[i] for i in labels])
     data_target = to_categorical(target_labels, class_mapping_n)
     data_input = images / 255
     return data_input, data_target
+
+def datagen(images, labels):
+    return ImageDataGenerator(rotation_range=10, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.1, zoom_range=0.1)
+
 
