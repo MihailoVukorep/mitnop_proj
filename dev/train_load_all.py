8ab11659d97fbf50da1507b2c17244871a13d22d
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-05-25 12:21:38 +0200

    train all free mem

--- dev/train_load_all.py
+++ dev/train_load_all.py
@@ -10,6 +10,7 @@ import cv2 as cv
 import random as rand
 import math
 import matplotlib.pyplot as plt
+import gc
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dropout, Dense
@@ -25,20 +26,23 @@ from dataset import *
 print("loading datasets...")
 
 train_images, train_labels, train_mapping = dataset_load_train()
-test_images, test_labels, test_mapping = dataset_load_test()
+#test_images, test_labels, test_mapping = dataset_load_test()
 
 train_mapping_n = len(train_mapping)
-test_mapping_n = len(test_mapping)
+#test_mapping_n = len(test_mapping)
 
 print("\n\n\n")
 
 print("loaded datasets:")
-print(f'train images: {train_images.shape}')
-print(f'train labels: {train_labels.shape}')
-print(f'train mapping: {train_mapping_n}')
-print(f'test images: {test_images.shape}')
-print(f'test labels: {test_labels.shape}')
-print(f'test mapping: {test_mapping_n}')
+print(f'train images..: {train_images.shape}')
+print(f'train labels..: {train_labels.shape}')
+print(f'train mapping.: {train_mapping_n}')
+print(f'train bytes...: {bytes_human_readable(train_images.nbytes)}')
+print()
+#print(f'test images...: {test_images.shape}')
+#print(f'test labels...: {test_labels.shape}')
+#print(f'test mapping..: {test_mapping_n}')
+#print(f'test bytes....: {bytes_human_readable(test_images.nbytes)}')
 print()
 
 # %% preprocess set for tf
@@ -109,15 +113,31 @@ class_mapping = {
     'Y': 60,
     'Z': 61
 }
+class_mapping_n = len(class_mapping)
 
 train_target_labels = np.array([class_mapping[i] for i in train_labels])
-test_target_labels = np.array([class_mapping[i] for i in test_labels])
+del train_labels
+gc.collect()
 
 train_input = train_images / 255
-train_target = to_categorical(train_target_labels, train_mapping_n)
+del train_images
+gc.collect()
 
-val_input = test_images / 255
-val_target = to_categorical(test_target_labels, test_mapping_n)
+train_target = to_categorical(train_target_labels, class_mapping_n)
+del train_target_labels
+gc.collect()
+
+# test_target_labels = np.array([class_mapping[i] for i in test_labels])
+# del test_labels
+# gc.collect()
+
+# test_input = test_images / 255
+# del test_images
+# gc.collect()
+
+# test_target = to_categorical(test_target_labels, class_mapping_n)
+# del test_target_labels
+# gc.collect()
 
 print("images prepared.")
 
@@ -132,7 +152,7 @@ model.add(Conv2D(filters=8, kernel_size=(3,3), strides=(1,1), padding="valid", a
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dropout(0.5))
-model.add(Dense(train_mapping_n, activation="softmax"))
+model.add(Dense(class_mapping_n, activation="softmax"))
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["categorical_accuracy"])
 model.summary()
 
@@ -144,7 +164,8 @@ batch_size = 100
 # %% train model
 
 print("training model...")
-cnn_results = model.fit(train_input, train_target, validation_data=(val_input, val_target), batch_size=batch_size, epochs=num_epochs, verbose=2)
+#cnn_results = model.fit(train_input, train_target, validation_data=(val_input, val_target), batch_size=batch_size, epochs=num_epochs, verbose=2)
+cnn_results = model.fit(train_input, train_target, batch_size=batch_size, epochs=num_epochs, verbose=2)
 print("model trained.")
 
 # %% save model
