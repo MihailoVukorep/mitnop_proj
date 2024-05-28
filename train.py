a8ed7d5d95e3fdb14baa7223d35cde59c5e962f5
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-05-28 09:01:16 +0200

    train log history

--- train.py
+++ train.py
@@ -4,13 +4,15 @@
 from utils_main import *
 from utils_load import *
 from utils_tf import *
+import pandas as pd
 
 # %% new model settings
 print("creating model: ", end='')
 batch_size = 10000
 num_epochs = 4
 model, name = create_model()
-model_name = f"all_{name}_batch{batch_size}_epoch{num_epochs}.keras"
+file_name = f"all_{name}_batch{batch_size}_epoch{num_epochs}"
+model_name = f"{file_name}.keras"
 print(model_name)
 
 # %% load set
@@ -29,11 +31,12 @@ print("images prepared.")
 
 # %% training
 print("training model...")
-history = model.fit(train_input, train_target, batch_size=batch_size, epochs=num_epochs, verbose=2)
+result = model.fit(train_input, train_target, batch_size=batch_size, epochs=num_epochs, verbose=2)
 print("model trained.")
 
 # %% save model
 print(f"saving model: {model_name}")
 model.save(d_models(model_name))
+pd.DataFrame(result.history).to_csv(d_models(f"{file_name}_history.csv"))
 print("model saved.")
 
