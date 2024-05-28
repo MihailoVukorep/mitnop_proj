c459ee1c462f9637ab55cfeb8e7f38e206b4e332
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-05-28 10:53:32 +0200

    fixed models selectiong

--- utils_selectmodel.py
+++ utils_selectmodel.py
@@ -4,17 +4,20 @@ from utils_main import *
 import os
 
 def selectmodel():
-
+    i = 0
     models = {}
-    for i, file in enumerate(os.listdir(models_dir)):
-        if not file.endswith(".keras"): continue
+    for file in os.listdir(models_dir):
+        if not file.endswith(".keras"):
+            continue
+        i += 1
         models[i] = file
+    print(models)
 
     if len(models) == 0:
         print("no models found")
         exit()
 
-    model_str = models[0]
+    model_str = models[1]
 
     if len(models) > 1:
         print("Please select a model:")
