c34be0505b601f5a996ae7e70107b33110d2b821
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-06-02 14:01:58 +0200

    fixed scripts and installation

--- test_draw.sh
+++ test_draw.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 source p3env/bin/activate
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Not inside a virtual environment. Exiting..."
+    exit 1
+else
+    echo "Inside a virtual environment. Proceeding..."
+fi
+
 python test_draw.py
